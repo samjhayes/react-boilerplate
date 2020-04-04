bd5e1cf65004e9ae99999345dd2b7e1233fe6225
Sam Hayes <sam.hayes@gamer-network.net>
2020-04-04 20:58:24 +0100

    Add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import { GlobalStyle } from './globalStyle';
 import { App } from './components/App';
 
 ReactDOM.render(
   <React.Fragment>
+    <GlobalStyle />
     <App target="world" />
   </React.Fragment>,
   document.getElementById('root'),
